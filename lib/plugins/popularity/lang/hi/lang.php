f1e57d9967e58d451b7ff884e7223696e4b8c9be
Alberto Valero-Gomez <alberto.valero.gomez@gmail.com>
2012-10-07 23:02:13 +0200

    Revert "dokuwiki"
    This reverts commit c3f9e188c1029d0f4c4423217a8adabc4d0fc1bb.

--- lib/plugins/popularity/lang/hi/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Hindi language file
- *
- * @author Abhinav Tyagi <abhinavtyagi11@gmail.com>
- * @author yndesai@gmail.com
- */
-$lang['submit']                = 'डेटा भेजे';
--- lib/plugins/popularity/lang/lt/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Lithuanian language file
- *
- * @author audrius.klevas@gmail.com
- * @author Arunas Vaitekunas <aras@fan.lt>
- */
-$lang['name']                  = 'Populiarumo apklausa (gali užtrukti pakrovimas)';
-$lang['submit']                = 'Pateikti';
