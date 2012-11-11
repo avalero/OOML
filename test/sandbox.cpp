c6580b0443e347e2342100f75c0583d05c7c4572
Alberto Valero <alberto.valero.gomez@gmail.com>
2012-11-11 12:33:23 +0100

    sandbox changes

--- test/sandbox.cpp
+++ test/sandbox.cpp
@@ -10,13 +10,8 @@ int main(int argc, char **argv)
 {
   IndentWriter writer;
 
-  Component cube = Cube(5, 15, 3);
-  Component cylinder1 = Cylinder(5,3).translate(-10,0,0);
-  Component cylinder2 = Cylinder(5,3).translate(10,0,0);
-  Component cylinder3 = Cylinder(5,3).translate(0,10,0);
-
-  Component hull = cube + cylinder1 + cylinder2 + cylinder3;
-  writer << hull;
+  Component cad = String("STRING",10,10,5);
+  writer << cad;
 
   ofstream file("sandbox.scad");
   file << writer;
