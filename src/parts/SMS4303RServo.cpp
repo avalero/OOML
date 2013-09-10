f779005ba11648ccd84b5c48a7eec1686406c4d5
avalero <alberto.valero@bqreaders.com>
2013-09-10 12:54:46 +0200

    stuff

---
+++ src/parts/SMS4303RServo.cpp
@@ -0,0 +1,84 @@
+#include "SMS4303RServo.h"
+#include <components/Cube.h>
+#include <core/Union.h>
+#include <core/Difference.h>
+#include <components/Cylinder.h>
+
+SMS4303RServo::SMS4303RServo(bool fill, bool cable_space) :
+    GenericServo(GenericServo::Data(),fill,cable_space)
+{
+    _data.width = 20.5;
+    _data.height = 39.5;
+    _data.length = 41.2;
+    _data.total_height_with_axis = 46.2;
+    _data.total_lenght_with_holes = 55.70;
+    _data.axis_radius = 5.9/2;
+    _data.axis_position = 11.50;
+    _data.holes_height = 30.24;
+    _data.holes_radius = 2.2;
+    _data.holes_support_thickness = 2.4;
+    _data.d_op_holes = 49;
+    _data.d_cl_holes = 10;
+    _data.cable_clearance_length = 7.81;
+    _data.cable_clearance_width = 7;
+
+    rebuild();
+}
+
+Component SMS4303RServo::getBackCover()
+{
+    double cover_height = 5.70;
+    double wall_thickness = 1.8;
+
+    Component cover = GenericServo::getBackCover();
+    Component body = Cube(_data.length,_data.width, cover_height)
+            - Cube(_data.length-2*wall_thickness , _data.width-2*wall_thickness , cover_height).translate(0,0,wall_thickness)
+            ;
+    body.translate(0,0,cover_height/2);
+
+
+    Component drills_around_tpl = Cylinder(2.4,cover_height,20,true).translate(0,0, cover_height/2);
+    Component drills_tpl = Cylinder(1.4,3*cover_height,20,true);
+    Component void_for_screws_tpl = Cylinder(3.2,1.5,20,true).translate(0,0, - _data.holes_support_thickness/2 + 1.5/2 - COMPONENT_MARGIN);
+
+    Component drills = drills_tpl.translatedCopy(-_data.length/2 + 2.3 , -_data.width/2 + 2.3 , 0)
+            + drills_tpl.translatedCopy(-_data.length/2 + 2.3 , _data.width/2 - 2.3, 0)
+            + drills_tpl.translatedCopy(_data.length/2 -2.3 , -_data.width/2 + 2.3, 0)
+            + drills_tpl.translatedCopy(_data.length/2 - 2.3 , _data.width/2 - 2.3 , 0)
+            ;
+
+    Component drills_around = drills_around_tpl.translatedCopy(-_data.length/2 + 2.3 , -_data.width/2 + 2.3 , 0)
+            + drills_around_tpl.translatedCopy(-_data.length/2 + 2.3 , _data.width/2 - 2.3, 0)
+            + drills_around_tpl.translatedCopy(_data.length/2 - 2.3 , -_data.width/2 + 2.3, 0)
+            + drills_around_tpl.translatedCopy(_data.length/2 - 2.3 , _data.width/2 - 2.3 , 0)
+            ;
+    Component void_for_screws = void_for_screws_tpl.translatedCopy(-_data.length/2 + 2.3 , -_data.width/2 + 2.3 , 0)
+            + void_for_screws_tpl.translatedCopy(-_data.length/2 + 2.3 , _data.width/2 - 2.3, 0)
+            + void_for_screws_tpl.translatedCopy(_data.length/2 - 2.3 , -_data.width/2 + 2.3, 0)
+            + void_for_screws_tpl.translatedCopy(_data.length/2 - 2.3 , _data.width/2 - 2.3 , 0)
+            ;
+
+    Component support_for_void_for_screws_tpl = Cylinder(3.2,1.9,20,true).translate(0,0, - _data.holes_support_thickness/2 + 1.9/2);
+
+
+    Component support_for_void_for_screws = support_for_void_for_screws_tpl.translatedCopy(-_data.length/2 + 2.3 , -_data.width/2 + 2.3 , 0)
+            + support_for_void_for_screws_tpl.translatedCopy(-_data.length/2 + 2.3 , _data.width/2 - 2.3, 0)
+            + support_for_void_for_screws_tpl.translatedCopy(_data.length/2 - 2.3 , -_data.width/2 + 2.3, 0)
+            + support_for_void_for_screws_tpl.translatedCopy(_data.length/2 - 2.3 , _data.width/2 - 2.3 , 0)
+            ;
+
+    Component wire_clearance = Cube(2*wall_thickness,_data.cable_clearance_width,cover_height).translate(_data.length/2 - wall_thickness/2 ,0,cover_height + _data.holes_support_thickness - 2);
+
+    cover = cover
+            + body
+            + drills_around
+            - drills
+            + support_for_void_for_screws
+            - void_for_screws
+            - wire_clearance
+            ;
+
+
+
+    return cover;
+}
---
+++ src/parts/SMS4303RServo.h
@@ -0,0 +1,66 @@
+/*
+*    OOML : Object Oriented Mechanics Library
+*    Copyright (C) 2012  Alberto Valero Gomez, Juan González Gómez, Rafael Treviño
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License (LGPL) as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License (LGPL) for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License (LGPL)
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef SMS4303R_SERVO_H_INCLUDED
+#define SMS4303R_SERVO_H_INCLUDED
+
+#ifdef WIN32
+#ifdef OOMLParts_EXPORTS
+#define OOMLParts_EXP_DEC __declspec(dllexport)
+#else
+#define OOMLParts_EXP_DEC __declspec(dllimport)
+#endif
+#else
+#define OOMLParts_EXP_DEC
+#endif
+
+#include "GenericServo.h"
+
+/**
+ * \brief Generic Servo Model
+ *
+ * This class provides a Generic Servo Model
+ */
+class OOMLParts_EXP_DEC SMS4303RServo : public GenericServo
+{
+public:
+
+    /* MEMBER FUNCTIONS */
+
+    /**
+     * @brief SM-S4303RServoServo
+     * @param fill
+     * @param cable_space
+     * @param crown
+     */
+    SMS4303RServo(bool fill = true, bool cable_space=true);
+
+    /**
+     * \brief Default destructor.
+     */
+    virtual ~SMS4303RServo() {}
+
+    /**
+     * @brief getBackCover
+     * @return BackCover of this precise servo model
+     */
+    virtual Component getBackCover();
+};
+
+#endif // OOML_GENERIC_SERVO_H_INCLUDED
